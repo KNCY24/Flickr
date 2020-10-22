43ce7fb558e2ad01dbfef91c4168b7e2f1a52613
android <kency.mariema@hotmail.com>
2020-10-22 12:09:42 +0200

    fix error mainviewmodel

--- app/src/main/java/com/e/flickr/ui/main/MainViewModel.kt
+++ app/src/main/java/com/e/flickr/ui/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.e.flickr.ui.main
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.e.flickr.model.Photo
+import com.e.flickr.model.Photos
 import com.e.flickr.model.SearchResult
 import com.e.flickr.repository.FlickrAPI
 import com.e.flickr.repository.Repository
@@ -17,18 +18,17 @@ class MainViewModel : ViewModel() {
     var position = 0
     val repository = Repository()
     val mldPhoto = MutableLiveData<Photo>()
-    val mlPhoto = mutableListOf<SearchResult>()
-    val lPhoto = mlPhoto.get(0).photos?.photo
+
     val callback = object:Callback<SearchResult>{
         override fun onFailure(call: Call<SearchResult>, t: Throwable) {
             print("Erreur callback")
         }
 
         override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-            var reponse = response.body()
-            if (reponse !=null && lPhoto != null) {
-                mlPhoto.add(reponse)
-                mldPhoto.value = lPhoto.get(position)
+           // val lPhoto = mlPhoto.get(0).photos?.photo
+            var reponse: SearchResult? = response.body()
+            if (response.isSuccessful) {
+                mldPhoto.value=  response.body()?.photos?.photo?.get(0)
             }
         }
     }
@@ -39,8 +39,5 @@ class MainViewModel : ViewModel() {
 
     fun nextPhoto() {
         position =+1
-        if (lPhoto != null) {
-            mldPhoto.value = lPhoto.get(position)
-        }
     }
 }
