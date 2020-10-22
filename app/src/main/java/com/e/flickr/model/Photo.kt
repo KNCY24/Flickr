9f634ff9ce6ab1f295f549e67eb2122107641d0f
android <kency.mariema@hotmail.com>
2020-10-22 12:11:47 +0200

    fix error mainviewmodel

--- app/src/main/java/com/e/flickr/model/Photo.kt
+++ app/src/main/java/com/e/flickr/model/Photo.kt
@@ -1,51 +1,6 @@
 package com.e.flickr.model
 
-class Photo{
-    var id: String ?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var owner : String?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var secret: String?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var server : String?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var farm: String?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var title: String?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var ispublic: Integer?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var isfirend: Integer?=null
-        get() = field
-        set(value) {
-            field = value
-        }
-    var isfamily: Integer?=null
-        get() = field
-        set(value) {
-            field = value
-        }
+class Photo(val id:String,val owner:String,val secret:String,val server:String,val farm:String,val title:String,val ispublic:Integer,val isfirend:Integer,val isfamily:Integer){
 
     override fun toString(): String {
         return "Photo(id='$id', owner='$owner', secret='$secret', server='$server', farm='$farm', title='$title', ispublic=$ispublic, isfirend=$isfirend, isfamily=$isfamily)"
